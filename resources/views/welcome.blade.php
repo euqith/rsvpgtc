073c70d83f707b1f3cd7c9957a83373101d25a2d
DESKTOP-Q7KC2MQ\EUXIA <christian.eyd@gmail.com>
2025-06-02 22:04:06 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,152 @@
+<!doctype html>
+<html lang="en" class="h-100 w-100">
+
+<head>
+    <title>RSVP GTC Easter</title>
+
+    {{-- <link rel="preload" href="img/GTClogo.png">
+    <link rel="preload" href="js/script.min.js" as="script"> --}}
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <!-- Required CSS files -->
+    <link href="css/style.min.css" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="icon" href="img/GTClogo.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="img/GTClogo.png" sizes="16x16" type="image/png">
+    <link rel="icon" href="img/gtcico.ico">
+</head>
+
+<body id="page-top" class="h-100 overflow-x-hidden w-100">
+
+    <!-- Preloader Start -->
+    {{-- <div class="preloader bg-white h-100 position-fixed w-100">
+        <svg width="100px" height="100px" viewBox="0 0 100 100" y="0px" x="0px" class="position-absolute start-50 top-50 translate-middle">
+            <g>
+                <path class="anim-heart" d="M89.49,37.8c0,25.54-39.59,46.24-39.59,46.24S10.31,63.34,10.31,37.8c0-29.59,39.59-28.67,39.59,0C49.9,10.06,89.49,8.21,89.49,37.8 z" stroke-miterlimit="10" stroke-linejoin="round" stroke-linecap="round" stroke-width="3.5" stroke="#000000" fill="#DDB796"></path>
+                <path class="anim-heart-sm" d="M89.63,70.9c-3.57,10.15-22.21,12.84-22.21,12.84s-12.84-13.77-9.26-23.92c4.14-11.76,19.75-5.86,15.74,5.54 C77.78,54.33,93.78,59.13,89.63,70.9z" stroke-miterlimit="10" stroke-linejoin="round" stroke-linecap="round" stroke-width="3.5" stroke="#000000" fill="#B46A48"></path>
+            </g>
+        </svg>
+    </div> --}}
+    <!-- Preloader End -->
+
+
+    <!-- Header Start -->
+    <header class="fixed-top">
+        <nav class="navbar navbar-expand-lg p-0">
+            <div class="container">
+                <!-- Navbar brand (mobile) -->
+                <div class="navbar-brand align-items-center d-flex d-lg-none me-0">
+                    <img width="100" height="100" src="img/GTClogo.png" alt="Logo">
+                </div>
+
+                <!-- Navbar toggler -->
+                {{-- <button class="navbar-toggler bg-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button> --}}
+
+                <!-- Navbar menu -->
+                {{-- <div id="navbar" class="collapse navbar-collapse justify-content-lg-center">
+                    <ul class="navbar-nav pt-3 pt-lg-0">
+                        <li class="nav-item me-3">
+                            <a class="nav-link page-scroll" href="#the-couple">The Couple</a>
+                        </li>
+                        <li class="nav-item me-3">
+                            <a class="nav-link page-scroll" href="#wedding-event">Events</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link page-scroll" href="#love-story">Love Story</a>
+                        </li>
+                    </ul> --}}
+
+                    <!-- Navbar brand (desktop) -->
+                    <div class="navbar-brand d-none d-lg-block mx-3 mx-xl-4 position-relative text-center">
+                        <img width="180" height="180" src="img/GTClogo.png" alt="Logo" class="mt-4 position-absolute start-0 top-0">
+                    </div>
+
+                    {{-- <ul class="navbar-nav">
+                        <li class="nav-item me-3">
+                            <a class="nav-link page-scroll" href="#photos">Photos</a>
+                        </li>
+                        <li class="nav-item me-3">
+                            <a class="nav-link page-scroll" href="#wedding-gifts">Wedding Gifts</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link page-scroll" href="#rsvp">RSVP</a>
+                        </li> --}}
+                    </ul>
+                </div>
+                <!-- //.collapse -->
+            </div>
+            <!-- //.container -->
+        </nav>
+    </header>
+    <!-- Header End -->
+
+
+    <!-- Spacer -->
+    <span class="spacer d-block w-100"></span>
+
+
+   
+
+
+    <!-- Section - The Couple Start -->
+    <section id="the-couple">
+        <div class="container position-relative z-2">
+            <div class="row mb-5 pb-lg-5">
+                <div class="col-12 position-relative text-center z-2">
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSckGyVYmhkNrPob-IwVcBgLgjWJzFoX_J3VUsKgAtIMT-1a_w/viewform?embedded=true" width="400" height="1227" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
+                    {{-- <span class="divider divider-arrow"></span> --}}
+                </div>
+                <!-- //.col-12 -->
+            </div>
+            <!-- //.row -->
+
+            
+            <!-- //.row -->
+        </div>
+        <!-- //.container -->
+
+        <!-- SVG Image -->
+        <img src="img/shape-wave-top-left-1.svg" alt="" class="shape-wave shape-wave-sm d-none d-lg-block mt-5 position-absolute start-0 top-0 z-1">
+        <img src="img/shape-wave-bottom-right-2.svg" alt="" class="shape-wave bottom-0 d-none d-lg-block end-0 position-absolute z-1">
+    </section>
+    <!-- Section - The Couple End -->
+
+
+    
+
+
+    
+
+
+    
+
+
+   
+
+
+    
+
+
+   
+
+
+   
+
+
+    <!-- Footer Start -->
+   
+
+
+    <!-- Required JS files -->
+    <script src="js/script.min.js"></script>
+</body>
+
+</html>
